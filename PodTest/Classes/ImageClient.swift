f3b8fda33aa1495fe3d2f1fa7f820fc1874a635c
Виталий Камашев <vkamashev@65apps.com>
2019-03-21 15:39:58 +0400

    refactoring

--- PodTest/Classes/ImageClient.swift
+++ PodTest/Classes/ImageClient.swift
@@ -2,9 +2,18 @@ import Alamofire
 
 
 public final class ImageClient {
+    enum CustomError: CustomNSError {
+        case unknown
+        case notValidImageLocation
+    }
+    
     public init() {}
     
-    public func image(with url: URL, completion: @escaping (CustomResult<UIImage>) -> ()) {
+    public func image(with url: URL? = nil, string: String? = nil, completion: @escaping (CustomResult<UIImage>) -> ()) {
+        guard let url = url ?? string.flatMap({ URL(string: $0) }) else {
+            return completion(.failure(CustomError.notValidImageLocation))
+        }
+        
         Alamofire.request(url).response { (response) in
             if let data = response.data, let image = UIImage(data: data) {
                 completion(.success(image))
--- PodTest/Classes/Result.swift
+++ PodTest/Classes/Result.swift
@@ -4,3 +4,19 @@ public enum CustomResult<ResultType> {
     case success(ResultType)
     case failure(Error?)
 }
+
+public extension CustomResult {
+    func map<T>(_ transform: (ResultType) -> T) -> CustomResult<T> {
+        switch self {
+        case let .success(value): return .success(transform(value))
+        case let .failure(error): return .failure(error)
+        }
+    }
+    
+    func flatMap<T>(_ transform: (ResultType) -> CustomResult<T>) -> CustomResult<T> {
+        switch self {
+        case let .success(value): return transform(value)
+        case let .failure(error): return .failure(error)
+        }
+    }
+}
